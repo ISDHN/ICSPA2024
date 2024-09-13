51f55ff286fb69393e1f9b98afb464024977880e
tracer-ics2024 <tracer@njuics.org>
2024-09-14 02:19:53 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:19:52 up  2:58,  1 user,  load average: 0.40, 0.24, 0.25

--- nemu/src/utils/elftool.c
+++ nemu/src/utils/elftool.c
@@ -27,9 +27,7 @@ int init_elf(const char *exec_file) {
 	}
 	read(elf, &ehdr, 0, sizeof(Elf32_Ehdr));
 	Elf32_Shdr shdrs[ehdr.e_shnum];
-	for (int i = 0; i < ehdr.e_shnum; i++) {
-		read(elf, &shdrs[i], ehdr.e_shoff + i * ehdr.e_shentsize, ehdr.e_shentsize);
-	}
+	read(elf, shdrs, ehdr.e_shoff, ehdr.e_shnum * ehdr.e_shentsize);
 	Elf32_Shdr shstrtab = shdrs[ehdr.e_shstrndx];
 
 	return shstrtab.sh_offset;
