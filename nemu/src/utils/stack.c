0d95307b362e19374fa199f7677ba362b9b1b4d8
tracer-ics2024 <tracer@njuics.org>
2024-09-15 00:20:22 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:20:22 up 16:34,  1 user,  load average: 0.80, 0.36, 0.24

--- nemu/src/utils/stack.c
+++ nemu/src/utils/stack.c
@@ -6,6 +6,10 @@ void push_int(int *stack, int *len, int val) {
 }
 
 int pop_int(int *stack, int *len) {
+	if (*len == 0) {
+		panic("Calling Stack is empty");
+		return -1;
+	}
 	(*len) -= 1;
 	return stack[*len];
 }
