dae4543a1d9cceb2f069a69284d1c48bc5a49e64
tracer-ics2024 <tracer@njuics.org>
2024-09-15 02:22:06 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:22:05 up 18:28,  1 user,  load average: 0.38, 0.18, 0.15

--- nemu/src/utils/elftool.c
+++ nemu/src/utils/elftool.c
@@ -94,7 +94,7 @@ char *find_func_name(vaddr_t inst_addr) {
 		return "Unknown";
 	}
 	for (int i = 0; i < func_num; i++) {
-		if (syms[func_list[i]].st_value <= inst_addr && inst_addr <= syms[func_list[i]].st_value + syms[func_list[i]].st_size) {
+		if (syms[func_list[i]].st_value <= inst_addr && inst_addr < syms[func_list[i]].st_value + syms[func_list[i]].st_size) {
 			return &symstrs[syms[func_list[i]].st_name];
 		}
 	}
