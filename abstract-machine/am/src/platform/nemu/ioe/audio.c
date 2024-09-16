f438e4eb6390dfb6850ad15b39e5c9f0677f711f
tracer-ics2024 <tracer@njuics.org>
2024-09-17 02:36:47 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:36:46 up 2 days, 13:16,  1 user,  load average: 0.30, 0.14, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,26 +1,39 @@
 #include <am.h>
 #include <nemu.h>
 
-#define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
-#define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
-#define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
+#define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)
+#define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)
+#define AUDIO_SAMPLES_ADDR (AUDIO_ADDR + 0x08)
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
-#define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
-#define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)
+#define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)
+
+uint32_t buffer_size = 0;
 
 void __am_audio_init() {
+	buffer_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+	cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+	cfg->present = true;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+	outl(AUDIO_FREQ_ADDR, ctrl->freq);
+	outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+	outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+	outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+	stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+	while (buffer_size - inl(AUDIO_COUNT_ADDR) < ctl->buf.end - ctl->buf.start)
+		;
+	for (uint8_t *i = ctl->buf.start; i < (uint8_t *)(ctl->buf.end); i++) {
+		outb(AUDIO_SBUF_SIZE_ADDR, *i);
+	}
 }
