7023114903ac039211f060c2d06129fa871939f2
tracer-ics2024 <tracer@njuics.org>
2024-09-16 23:58:32 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:58:31 up 2 days, 10:43,  1 user,  load average: 0.38, 0.25, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,13 +20,13 @@ void __am_gpu_status(AM_GPU_STATUS_T *status) {
 }
 
 void __am_gpu_init() {
-	// int i;
-	// AM_GPU_CONFIG_T cfg;
-	// __am_gpu_config(&cfg);
-	// int w = cfg.width;	// TODO: get the correct width
-	// int h = cfg.height; // TODO: get the correct height
-	// uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	// for (i = 0; i < w * h; i++)
-	// 	fb[i] = i;
-	// outl(SYNC_ADDR, 1);
+	int i;
+	AM_GPU_CONFIG_T cfg;
+	__am_gpu_config(&cfg);
+	int w = cfg.width;	// TODO: get the correct width
+	int h = cfg.height; // TODO: get the correct height
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	for (i = 0; i < w * h; i++)
+		fb[i] = i;
+	outl(SYNC_ADDR, 1);
 }
