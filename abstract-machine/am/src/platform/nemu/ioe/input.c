1293bbb8bfc83457649d5f36998bf59a4cb087ec
tracer-ics2024 <tracer@njuics.org>
2024-09-16 23:52:15 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:52:15 up 2 days, 10:37,  1 user,  load average: 0.33, 0.16, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,5 +6,5 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	uint32_t code = inl(KBD_ADDR);
 	kbd->keydown = code > KEYDOWN_MASK;
-	kbd->keycode = ~AM_KEY_NONE & code;
+	kbd->keycode = ~KEYDOWN_MASK & code;
 }
