68f249509a50a0fd279071fb00d81157768ee502
tracer-ics2024 <tracer@njuics.org>
2024-09-17 23:55:11 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:55:11 up 3 days,  9:44,  1 user,  load average: 0.36, 0.20, 0.12

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -16,7 +16,8 @@
 #ifndef __CPU_IFETCH_H__
 
 #include <memory/vaddr.h>
-inline uint32_t inst_fetch(vaddr_t *pc, int len) {
+
+static uint32_t inst_fetch(vaddr_t *pc, int len) {
 	uint32_t inst = vaddr_ifetch(*pc, len);
 	(*pc) += len;
 	return inst;
--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -25,7 +25,7 @@ LD := $(CXX)
 LEX := flex
 YACC := bison
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+CFLAGS  := -O2 -MMD  -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 -flto $(LDFLAGS)
 
 LEX_C = $(LEXES:%.l=%.yy.c)
