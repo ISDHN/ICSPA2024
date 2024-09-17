a09722cbb2e429e5c07cc76953f43f88c95ac11f
tracer-ics2024 <tracer@njuics.org>
2024-09-17 12:23:38 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:23:38 up 2 days, 22:38,  1 user,  load average: 0.34, 0.18, 0.19

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -36,6 +36,9 @@ static uint32_t pos_l = 0, pos_r = 0;
 static void callback(void *userdata, uint8_t *stream, int len) {
 	memset(stream, 0, len);
 	int count = pos_r - pos_l;
+	while (count < 1024) {
+		count = pos_r - pos_l;
+	}
 	if (count > len) {
 		count = len;
 	}
