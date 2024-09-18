7a95996c024da472462192ac335d578a765f0e51
tracer-ics2024 <tracer@njuics.org>
2024-09-18 22:10:40 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:10:39 up 35 min,  1 user,  load average: 0.58, 0.52, 0.43

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -21,6 +21,8 @@
 typedef struct {
 	word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)]; // general purpose registers
 	vaddr_t pc;
+	word_t mt_csr[256];
+	word_t *csr[16];
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
@@ -29,6 +31,31 @@ typedef struct {
 		uint32_t val;
 	} inst;
 } MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
+
+enum mt_csr_cst { // mechine trap csr
+	mstatus = 0,
+	misa,
+	medeleg,
+	mideleg,
+	mie,
+	mtvec,
+	mcounteren,
+	mstatush,
+	mscratch = 0x40,
+	mepc,
+	mcause,
+	mtval,
+	mip,
+	mtinst,
+	mtval2
+};
+
+enum csr_kind_cst {
+	mrw = 3,
+	mro = 15
+};
+
+#define CSR(imm) cpu.csr[imm >> 8][imm | 0xff]
 
 #define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
 
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -16,10 +16,8 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-	/* TODO: Trigger an interrupt/exception with ``NO''.
-	 * Then return the address of the interrupt/exception vector.
-	 */
-
+	cpu.csr[mrw][mepc] = epc;
+	cpu.pc = cpu.csr[mrw][mtvec];
 	return 0;
 }
 
