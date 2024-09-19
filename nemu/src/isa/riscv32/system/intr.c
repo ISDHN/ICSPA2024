4356844009b3ff14c0b36df2be68cf920b8168da
tracer-ics2024 <tracer@njuics.org>
2024-09-19 13:20:48 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:20:48 up  7:19,  1 user,  load average: 0.25, 0.23, 0.19

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,8 +18,8 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	cpu.csr[mrw][mepc] = epc;
 #ifdef CONFIG_ETRACE
-	Log("Interrupt: NO = %d, epc = %#x \n", NO, epc);
-	Log("Go to interrupt handler: %#x \n", cpu.csr[mrw][mtvec]);
+	Log("Interrupt: NO = %d, epc = %#x", NO, epc);
+	Log("Go to interrupt handler: %#x", cpu.csr[mrw][mtvec]);
 #endif
 	return cpu.csr[mrw][mtvec];
 }
