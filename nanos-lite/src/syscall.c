112db83b1281b0e2b75cf749112e2d56f91eb1d0
tracer-ics2024 <tracer@njuics.org>
2024-09-21 00:31:55 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:31:55 up 1 day,  3:02,  1 user,  load average: 0.16, 0.23, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,19 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
+void sys_yield() {
+	yield();
+}
+
+void do_syscall(Context *c) {
+	uintptr_t a[4];
+	a[0] = c->GPR1;
+
+	switch (a[0]) {
+		case SYS_yield:
+			sys_yield();
+			break;
+		default:
+			panic("Unhandled syscall ID = %d", a[0]);
+	}
 }
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -17,7 +17,7 @@
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	cpu.csr[mrw][mepc] = epc;
-	cpu.csr[mrw][mcause] = NO;
+	cpu.csr[mrw][mcause] = -1;
 #ifdef CONFIG_ETRACE
 	Log("Interrupt: NO = %d, epc = %#x, mcause = %#x", NO, epc, cpu.csr[mrw][mcause]);
 	Log("Go to interrupt handler: %#x", cpu.csr[mrw][mtvec]);
