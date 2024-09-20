5e9e85611fa6510d1c12f03a815012f2eaa75368
tracer-ics2024 <tracer@njuics.org>
2024-09-21 01:05:26 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:05:26 up 1 day,  3:35,  1 user,  load average: 0.41, 0.29, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,10 +8,16 @@ void sys_yield() {
 void do_syscall(Context *c) {
 	uintptr_t a[4];
 	a[0] = c->GPR1;
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
 	switch (a[0]) {
+		case SYS_exit:
+			halt(a[1]);
 		case SYS_yield:
 			sys_yield();
+			c->GPRx = 0;
 			break;
 		default:
 			panic("Unhandled syscall ID = %d", a[0]);
