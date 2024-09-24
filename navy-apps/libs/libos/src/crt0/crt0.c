e0070bc15286c88fbfb519e1361460f891a79cee
tracer-ics2024 <tracer@njuics.org>
2024-09-24 20:05:44 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:05:44 up  6:58,  1 user,  load average: 0.55, 0.38, 0.34

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,24 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
+// #define AM
+
+#ifdef AM
+int main(const char *args);
+#else
 int main(int argc, char *argv[], char *envp[]);
+#endif
 extern char **environ;
 void call_main(uintptr_t *args) {
 	char *empty[] = {NULL};
+	char *am_arg = "mario";
 	environ = empty;
+#ifdef AM
+	exit(main(am_arg));
+#else
 	exit(main(0, empty, empty));
+#endif
 	assert(0);
 }
