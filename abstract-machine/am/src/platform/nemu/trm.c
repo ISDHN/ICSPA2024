b29cf52996f1e4d535bbb8d1f465bdbc92cc0e1b
tracer-ics2024 <tracer@njuics.org>
2024-09-24 19:15:51 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:15:51 up  6:07,  1 user,  load average: 0.30, 0.25, 0.20

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -8,17 +8,18 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+	outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
-  nemu_trap(code);
+	nemu_trap(code);
 
-  // should not reach here
-  while (1);
+	// should not reach here
+	while (1)
+		;
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
-  halt(ret);
+	int ret = main(mainargs);
+	halt(ret);
 }
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-	char *empty[] = {"ref"};
+	char *empty[] = {NULL};
 	environ = empty;
 	exit(main(0, empty, empty));
 	assert(0);
