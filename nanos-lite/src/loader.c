ae3e1ce2096b64307959e993cca34863c3584e40
tracer-ics2024 <tracer@njuics.org>
2024-09-24 19:08:14 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:08:14 up  6:00,  1 user,  load average: 0.21, 0.14, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -50,5 +50,5 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 void naive_uload(PCB *pcb, const char *filename) {
 	uintptr_t entry = loader(pcb, filename);
 	Log("Jump to entry = %p", entry);
-	((void (*)(char *))entry)("ref");
+	((void (*)())entry)();
 }
