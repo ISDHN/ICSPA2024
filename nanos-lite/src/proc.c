e72d5dc63c076a3ceaaacb1dec1d2fc9e642706f
tracer-ics2024 <tracer@njuics.org>
2024-09-25 23:48:15 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:48:15 up 1 day,  9:50,  1 user,  load average: 0.93, 0.99, 0.85

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
 	Log("Initializing processes...");
 
 	// load program here
-	char *filename = "/bin/pal";
+	char *filename = "/bin/nplayer";
 	Log("Loading program from %s", filename);
 	naive_uload(NULL, filename);
 }
