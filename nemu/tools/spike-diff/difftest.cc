7aa1afe6af528fdceb9b84a743318855af31882a
tracer-ics2024 <tracer@njuics.org>
2024-09-26 11:30:04 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:30:04 up 1 day, 21:28,  1 user,  load average: 0.31, 0.22, 0.26

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -91,10 +91,10 @@ void sim_t::diff_set_regs(void *diff_context) {
 	for (int i = 0; i < NR_GPR; i++) {
 		state->XPR.write(i, (sword_t)ctx->gpr[i]);
 	}
-	state->mepc = ctx->csr[mrw][mepc];
-	state->mstatus = ctx->csr[mrw][mstatus];
-	state->mcause = ctx->csr[mrw][mcause];
-	state->mtvec = ctx->csr[mrw][mtvec];
+	*(state->mepc) = ctx->csr[mrw][mepc];
+	*(state->mstatus) = ctx->csr[mrw][mstatus];
+	*(state->mcause) = ctx->csr[mrw][mcause];
+	*(state->mtvec) = ctx->csr[mrw][mtvec];
 	state->pc = ctx->pc;
 }
 
