98956500138cb3f903b09f451ac3c0cd59e53f84
tracer-ics2024 <tracer@njuics.org>
2024-09-26 21:01:22 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:01:22 up 2 days,  3:42,  1 user,  load average: 0.25, 0.45, 0.59

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -31,11 +31,10 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-	int buf_size = (uint8_t *)ctl->buf.end - (uint8_t *)ctl->buf.start;
-	uint8_t *buf = ctl->buf.start;
+	int buf_size = ctl->buf.end - ctl->buf.start;
 	while (buffer_size - inl(AUDIO_COUNT_ADDR) < buf_size)
 		;
-	for (int i = 0; i < buf_size; i++) {
-		outb(AUDIO_SBUF_ADDR, buf[i]);
+	for (uint8_t *i = ctl->buf.start; i < (uint8_t *)(ctl->buf.end); i++) {
+		outb(AUDIO_SBUF_ADDR, *i);
 	}
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -72,12 +72,12 @@ static void audio_buffer_handle(uint32_t offset, int len, bool is_write) {
 	assert(len == 1);
 	assert(offset == 0);
 	if (is_write) {
+		sbuf[pos_r] = temp_buf[0];
 		while (CONFIG_SB_SIZE - get_count() <= 256) {
 #ifdef CONFIG_WARN_OVERFLOW
 			Warning("audio buffer near overflow");
 #endif
 		}
-		sbuf[pos_r] = temp_buf[0];
 		pos_r = (pos_r + 1) % CONFIG_SB_SIZE;
 #ifdef CONFIG_WARN_OVERFLOW
 		if (pos_r == pos_l) {
