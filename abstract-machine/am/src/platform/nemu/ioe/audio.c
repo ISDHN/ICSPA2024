f9c8d6f80e3d8efc15b3814e78e4505793c735e0
tracer-ics2024 <tracer@njuics.org>
2024-09-26 20:01:51 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:01:51 up 2 days,  2:45,  1 user,  load average: 0.34, 0.33, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -32,8 +32,8 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	int buf_size = (uint8_t *)ctl->buf.end - (uint8_t *)ctl->buf.start;
-	while (buffer_size - inl(AUDIO_COUNT_ADDR) < buf_size)
-		;
+	// while (buffer_size - inl(AUDIO_COUNT_ADDR) < buf_size)
+	// 	;
 	int i = 0;
 	// for (; i < buf_size; i += 4) {
 	// 	outl(AUDIO_SBUF_ADDR, *((uint32_t *)(ctl->buf.start) + i));
