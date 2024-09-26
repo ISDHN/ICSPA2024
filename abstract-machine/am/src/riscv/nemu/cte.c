d1b19598b9dc6286fe0c74b0028951c84cb70f56
tracer-ics2024 <tracer@njuics.org>
2024-09-26 23:00:00 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:00:00 up 2 days,  5:37,  1 user,  load average: 0.28, 0.19, 0.20

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -40,7 +40,7 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 	Context *c = (Context *)kstack.end - 1;
-	c->mepc = (uintptr_t)entry;
+	c->mepc = (uintptr_t)entry - 4;
 	*((Context **)kstack.start) = c;
 	return c;
 }
