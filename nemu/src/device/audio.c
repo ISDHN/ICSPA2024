af64a858982fe9ac47d2ccd09645ec6f82eb3e21
tracer-ics2024 <tracer@njuics.org>
2024-09-26 21:33:34 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:33:34 up 2 days,  4:13,  1 user,  load average: 1.04, 0.84, 0.63

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -72,8 +72,8 @@ static void audio_buffer_handle(uint32_t offset, int len, bool is_write) {
 	assert(len == 1);
 	assert(offset == 0);
 	if (is_write) {
-		while (CONFIG_SB_SIZE - get_count() < 256)
-			printf("w");
+		while (CONFIG_SB_SIZE - get_count() < audio_base[reg_samples] * 16)
+			;
 
 		sbuf[pos_r] = temp_buf[0];
 		pos_r = (pos_r + 1) % CONFIG_SB_SIZE;
