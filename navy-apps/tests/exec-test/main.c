8266f24834d00fc0bf846252672b9aea782cb6cb
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:20:27 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:20:27 up 3 days,  6:00,  1 user,  load average: 0.17, 0.28, 0.22

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -6,11 +6,10 @@
 int main(int argc, char *argv[]) {
 	int flag = 0;
 	int n = (argc >= 2 ? atoi(argv[1]) : 1);
-	printf("%s: argv[1] = %d flag: %d\n", argv[0], n, flag);
 
 	char buf[16];
 	sprintf(buf, "%d", n + 1);
 	flag = 1;
-	execl(argv[0], argv[0], buf, NULL);
+	execl("/bin/nterm", "/bin/nterm", NULL);
 	return 0;
 }
