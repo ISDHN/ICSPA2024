7056e0676612d033ebd0fc1cbf5ab6c0f7520351
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:33:29 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:33:29 up 3 days,  6:13,  1 user,  load average: 0.11, 0.19, 0.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,7 +14,6 @@ static const char *keyname[256] __attribute__((used)) = {
 	AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-	yield();
 	for (size_t i = 0; i < len; i++) {
 		putch(((char *)buf)[i]);
 	}
