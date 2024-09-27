02470318a3b6e688149c61f494f66b7ac44346b5
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:08:33 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:08:33 up 3 days,  5:48,  1 user,  load average: 0.24, 0.23, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -58,6 +58,7 @@ int sys_gettimeofday(struct timeval *tv, void *tz) {
 
 int sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
 	int res = context_uload(pathname, argv, envp);
+	printf("res = %d\n", res);
 	if (!res) {
 		yield();
 	}
