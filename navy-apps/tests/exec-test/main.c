7193ba73abcb6fa699af8f54f0ced8baaccbb545
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:18:09 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:18:09 up 3 days,  5:57,  1 user,  load average: 0.47, 0.33, 0.23

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,12 +4,13 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello, World!\n");
+	int flag = 0;
 	int n = (argc >= 2 ? atoi(argv[1]) : 1);
-	printf("%s: argv[1] = %d\n", argv[0], n);
+	printf("%s: argv[1] = %d\n flag: %d", argv[0], n, flag);
 
 	char buf[16];
 	sprintf(buf, "%d", n + 1);
+	flag = 1;
 	execl(argv[0], argv[0], buf, NULL);
 	return 0;
 }
