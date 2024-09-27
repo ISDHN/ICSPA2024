59e009b966db2ff6506679b569c1d85ba7aa1f9a
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:06:09 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:06:09 up 3 days,  5:45,  1 user,  load average: 0.15, 0.16, 0.17

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,9 +4,8 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	printf("Test: argc = d\n");
 	int n = (argc >= 2 ? atoi(argv[1]) : 1);
-	printf("%s: argv[1] = %d\n", argv[0], n);
+	printf("%s-%d: argv[1] = %d\n", argv[0], argc, n);
 
 	char buf[16];
 	sprintf(buf, "%d", n + 1);
