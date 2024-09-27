7154fe8ada83b7687775c3095e65102b29914d90
tracer-ics2024 <tracer@njuics.org>
2024-09-28 02:38:31 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:38:31 up 3 days,  5:18,  1 user,  load average: 0.18, 0.34, 0.30

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -82,6 +82,7 @@ int context_uload(const char *filename, char *const argv[], char *const envp[]) 
 
 	pcb[pcb_count].cp->GPRx = (uintptr_t)ustack;
 	pcb_count++;
+	switch_boot_pcb();
 	return 0;
 }
 
@@ -122,7 +123,7 @@ Context *schedule(Context *prev) {
 		printf("next pcb:%d\n", next - pcb);
 		current = next;
 	} else {
-		if (pcb[pcb_count].cp != NULL) {
+		if (pcb[pcb_count - 1].cp != NULL) {
 			printf("next pcb:%d\n", pcb_count);
 			current = pcb;
 		}
