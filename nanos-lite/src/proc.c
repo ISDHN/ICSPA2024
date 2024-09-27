b74c7679727fb2e5f6f6b4f76c1e8c97f8746055
tracer-ics2024 <tracer@njuics.org>
2024-09-28 03:00:21 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:00:21 up 3 days,  5:40,  1 user,  load average: 0.11, 0.15, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -84,6 +84,7 @@ int context_uload(const char *filename, char *const argv[], char *const envp[]) 
 	// PUSH(filename_ptr, char *);
 	PUSH(argc, int); // and the program name
 
+	printf("OS: argc = %d\n", argc);
 	pcb[pcb_count].cp->GPRx = (uintptr_t)ustack;
 	pcb_count++;
 	switch_boot_pcb();
