f9c0b12ee66a7bffc2df483c32bd4708b722dc9f
tracer-ics2024 <tracer@njuics.org>
2024-09-29 16:48:26 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  16:48:26 up 18:20,  0 user,  load average: 0.15, 0.27, 0.39

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,6 +37,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 static int screen_w;
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+	printf("update vga");
 	int pix_idx = offset / sizeof(uint32_t);
 	int x = pix_idx % screen_w;
 	int y = pix_idx / screen_w;
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -78,6 +78,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+
 	// audio_callback_caller();
 	assert(dst);
 
