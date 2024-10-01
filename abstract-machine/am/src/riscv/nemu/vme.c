79646c421afb559be0aff5c8f8ea120f03332d8a
tracer-ics2024 <tracer@njuics.org>
2024-10-01 10:47:38 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  10:47:38 up 1 day, 23:57,  0 user,  load average: 0.37, 0.25, 0.16

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -80,7 +80,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	uintptr_t root = (uintptr_t)as->ptr;
 	vaddr_ena vaddr = {.val = (uintptr_t)va};
 	paddr_ena paddr = {.val = (uintptr_t)pa};
-	printf("ppn: %x\n", paddr.ppn_1);
+	printf("ppn: %x\n", paddr.val);
 	if (as->area.start == USER_SPACE.start && as->area.end == USER_SPACE.end) {
 
 	} else {
