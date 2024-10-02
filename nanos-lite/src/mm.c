4650c7f73f2a1dadfab3cdcfb7f6aeb3d4a80b3c
tracer-ics2024 <tracer@njuics.org>
2024-10-02 15:00:49 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  15:00:49 up 16:02,  0 user,  load average: 0.37, 0.32, 0.24

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -28,6 +28,7 @@ int mm_brk(uintptr_t brk) {
 	uint32_t new_vpage = brk >> PAGE_SHIFT;
 	if (old_vpage != new_vpage) {
 		void *new_ppage = new_page(1);
+		Log("brk: %p -> %p", old_vpage, new_vpage);
 		map(&current->as, (void *)(new_vpage), new_ppage, 0);
 	}
 	current->max_brk = brk;
