7a85752f0f62220ab711e9ce3f0be09783929fff
tracer-ics2024 <tracer@njuics.org>
2024-10-02 18:06:31 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  18:06:31 up 18:22,  0 user,  load average: 0.63, 0.49, 0.45

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -73,7 +73,7 @@ void __am_get_cur_as(Context *c) {
 void __am_switch(Context *c) {
 	if (vme_enable && c->pdir != NULL) {
 		set_satp(c->pdir);
-		printf("switch satp to: %p\n", c->pdir);
+		// printf("switch satp to: %p\n", c->pdir);
 	}
 }
 
