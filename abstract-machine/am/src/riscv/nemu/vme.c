872d427072059c7c1c732e618141b82243185537
tracer-ics2024 <tracer@njuics.org>
2024-10-02 16:31:10 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  16:31:10 up 17:25,  0 user,  load average: 0.41, 0.30, 0.27

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -99,6 +99,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 		next_lvl->w = 1;
 		next_lvl->ex = 1;
 		next_lvl->valid = 1;
+		printf("Map %p to %p\n", va, pa);
 	} else {
 		pte->ppn = ((uintptr_t)pa >> PAGE_SHIFT) & (~PN_MASK);
 		pte->r = 1;
@@ -106,7 +107,6 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 		pte->ex = 1;
 		pte->valid = 1;
 	}
-	printf("Map %p to %p\n", va, pa);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
