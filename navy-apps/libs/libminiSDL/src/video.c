3a9dbd2ba5f684c000670e16f99f38522edd0e6f
tracer-ics2024 <tracer@njuics.org>
2024-10-03 17:39:52 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  17:39:52 up 1 day, 40 min,  0 user,  load average: 0.40, 0.37, 0.39

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -80,7 +80,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-	return;
+
 	// audio_callback_caller();
 	assert(dst);
 
