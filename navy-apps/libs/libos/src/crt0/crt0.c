1b431c8de5200f9f2725be6d0dcf0fa98021a0f5
tracer-ics2024 <tracer@njuics.org>
2024-10-03 08:37:50 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  08:37:50 up 22:15,  0 user,  load average: 0.49, 0.44, 0.32

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -19,7 +19,7 @@ void call_main(uintptr_t args) {
 	char **argv = (char **)(args + sizeof(int));
 	environ = argv + argc + 1;
 	__libc_init_array();
-	printf("args: %#x\nargc: %p\n", args, &argc);
+	printf("args: %#x\n", args);
 #ifdef AM
 	exit(main(am_arg))
 #else
