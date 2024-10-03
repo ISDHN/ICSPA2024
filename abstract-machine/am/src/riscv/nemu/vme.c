29cf7dc4aab9832801e613f838507cdc263af40a
tracer-ics2024 <tracer@njuics.org>
2024-10-03 20:34:41 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  20:34:41 up 1 day,  3:35,  0 user,  load average: 0.24, 0.19, 0.18

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -78,8 +78,8 @@ void __am_switch(Context *c) {
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-	printf("Map %p to %p\n", va, pa);
 	uintptr_t root = (uintptr_t)as->ptr;
+	printf("pdir: %p\n", root);
 	vaddr_ena vaddr = {.val = (uintptr_t)va};
 	pte_t *pte = (pte_t *)(root + (vaddr.vpn_1 << PTE_SHIFT));
 	if (pte->val == 0) {
