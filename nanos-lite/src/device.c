e498a0300b75750a494d45eb6c9deef9c9f1e274
tracer-ics2024 <tracer@njuics.org>
2024-10-04 19:42:28 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  19:42:28 up 1 day, 18:17,  0 user,  load average: 0.16, 0.26, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,6 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
 	for (size_t i = 0; i < len; i++) {
-		// yield();
 		putch(((char *)buf)[i]);
 	}
 	return len;
@@ -23,7 +22,6 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
 	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-	// yield();
 	if (ev.keycode == AM_KEY_NONE) {
 		return 0;
 	}
@@ -39,7 +37,6 @@ static int screen_w;
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 	int pix_idx = offset / sizeof(uint32_t);
 	int x = pix_idx % screen_w;
-	// yield();
 	int y = pix_idx / screen_w;
 	int l = len / sizeof(uint32_t);
 
@@ -66,7 +63,6 @@ size_t sbctrl_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t sb_write(const void *buf, size_t offset, size_t len) {
-	// yield();
 	char *p = (char *)buf;
 	io_write(AM_AUDIO_PLAY, {p, p + len});
 	return len;
