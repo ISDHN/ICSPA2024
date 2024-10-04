ece862e07eee0a4447afe4d2b65da5a385c806ca
tracer-ics2024 <tracer@njuics.org>
2024-10-04 17:59:51 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  17:59:51 up 1 day, 16:34,  0 user,  load average: 0.19, 0.17, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c) {
 	switch (e.event) {
 		case EVENT_IRQ_TIMER:
-			//			Log("Tik tak...");
+			Log("Tik tak...");
 		case EVENT_YIELD:
 			c = schedule(c);
 			break;
