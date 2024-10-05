be25af8c0d57be2b07f39f9955b42857f24a0778
tracer-ics2024 <tracer@njuics.org>
2024-10-05 10:06:22 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  10:06:22 up 2 days, 23 min,  0 user,  load average: 0.32, 0.35, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,13 +17,13 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 	for (size_t i = 0; i < len; i++) {
 		putch(((char *)buf)[i]);
 	}
-	// yield();
+	yield();
 	return len;
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
 	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-	// yield();
+	yield();
 	if (ev.keycode == AM_KEY_NONE) {
 		return 0;
 	}
@@ -39,7 +39,7 @@ static int screen_w;
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 	int pix_idx = offset / sizeof(uint32_t);
 	int x = pix_idx % screen_w;
-	// yield();
+	yield();
 	int y = pix_idx / screen_w;
 	int l = len / sizeof(uint32_t);
 
