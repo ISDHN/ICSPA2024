cd0bef1b17a7b94f7230aef2a557dcceb2e99e97
tracer-ics2024 <tracer@njuics.org>
2024-10-05 09:38:29 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  09:38:29 up 1 day, 23:56,  0 user,  load average: 0.25, 0.16, 0.13

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -37,10 +37,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
-	// mstatus_t *status = get_mstatus();
-	// if (status->mie && cpu.intr) {
-	// 	cpu.intr = false;
-	// 	return IRQ_TIMER;
-	// }
+	mstatus_t *status = get_mstatus();
+	if (status->mie && cpu.intr) {
+		cpu.intr = false;
+		return IRQ_TIMER;
+	}
 	return INTR_EMPTY;
 }
