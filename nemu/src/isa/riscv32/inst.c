d31044e5e5cf527d72bcc86699b861ab30daba8c
tracer-ics2024 <tracer@njuics.org>
2024-10-05 08:17:58 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  08:17:58 up 1 day, 22:35,  0 user,  load average: 0.21, 0.29, 0.24

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -119,10 +119,10 @@ static inline void Mret(Decode *s) {
 	status->mie = status->mpie;
 	status->mpie = 1;
 	if (cpu.mt_csr[mcause] == 0x80000007) {
-		s->dnpc -= 4;
-		// printf("epc: %#x\n", MRW(mepc));
-		// isa_reg_display();
-		// Log("end timer intr");
+		// s->dnpc -= 4;
+		//  printf("epc: %#x\n", MRW(mepc));
+		//  isa_reg_display();
+		//  Log("end timer intr");
 	}
 }
 
