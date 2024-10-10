39ac9b5b0aed793244c302f214361bc7c42b1f63
tracer-ics2024 <tracer@njuics.org>
2024-10-10 11:19:29 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  11:19:29 up 3 days, 28 min,  0 user,  load average: 0.66, 0.49, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -13,7 +13,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *);
 void __am_audio_config(AM_AUDIO_CONFIG_T *);
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *);
 void __am_audio_status(AM_AUDIO_STATUS_T *);
-void __am_audio_play(AM_AUDIO_PLAY_T *) __attribute((optimize(0)));
+void __am_audio_play(AM_AUDIO_PLAY_T *);
 void __am_disk_config(AM_DISK_CONFIG_T *cfg);
 void __am_disk_status(AM_DISK_STATUS_T *stat);
 void __am_disk_blkio(AM_DISK_BLKIO_T *io);
