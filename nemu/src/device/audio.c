6d0779f05213114ec5685aadf1597fb9cff66b15
tracer-ics2024 <tracer@njuics.org>
2024-10-10 10:58:32 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  10:58:32 up 3 days, 7 min,  0 user,  load average: 0.31, 0.29, 0.34

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -72,13 +72,16 @@ static void audio_buffer_handle(uint32_t offset, int len, bool is_write) {
 	assert(len == 1);
 	assert(offset == 0);
 	if (is_write) {
-		// 		while (CONFIG_SB_SIZE - get_count() < audio_base[reg_samples] * 16) {
-		// #ifdef CONFIG_WARN_OVERFLOW
-		// 			Warning("audio buffer near overflow. free space = %d", CONFIG_SB_SIZE - get_count());
-		// #endif
-		// 		}
+		while (CONFIG_SB_SIZE - get_count() < audio_base[reg_samples] * 16)
+			Warning("audio buffer near overflow");
+
 		sbuf[pos_r] = temp_buf[0];
 		pos_r = (pos_r + 1) % CONFIG_SB_SIZE;
+#ifdef CONFIG_WARN_OVERFLOW
+		if (pos_r == pos_l) {
+			Warning("audio buffer overflow");
+		}
+#endif
 	}
 }
 
