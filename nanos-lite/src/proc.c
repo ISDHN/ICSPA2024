812c15a3a9bd49a3879ca249f4c9b8433cdf4ad6
tracer-ics2024 <tracer@njuics.org>
2024-10-28 21:42:08 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  21:42:08 up 44 min,  2 users,  load average: 0.19, 0.15, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -126,6 +126,7 @@ int context_uload(const char *filename, char *const argv[], char *const envp[], 
 	void *entry = (void *)loader(dst_pcb, filename);
 	dst_pcb->cp = ucontext(&dst_pcb->as, (Area){dst_pcb->stack, dst_pcb->stack + STACK_SIZE}, entry);
 	dst_pcb->cp->GPRx = (uintptr_t)ustack;
+	Log("Program arg: %x", ustack);
 	dst_pcb->max_brk = 0;
 	if (priority != -1) {
 		dst_pcb->priority = priority;
