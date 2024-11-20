737027e57790491b2a1e7665d25ef7462c50d142
tracer-ics2024 <tracer@njuics.org>
2024-11-20 14:54:04 +0800

    >  compile NEMU 231250186 杨承岳 Linux LAPTOP-NM9E0FR1 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 GNU/Linux  14:54:04 up 3 min,  2 users,  load average: 0.47, 0.38, 0.16

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -144,7 +144,6 @@ void hello_fun(void *arg) {
 
 void init_proc() {
 	char *init_program = "/bin/nterm";
-	char *sec_program = "/bin/pal";
 	// context_kload(hello_fun, "arg10", 1);
 	context_uload("/bin/hello", (char *[]){"/bin/hello", NULL}, NULL, 1, true);
 	register_bg();
