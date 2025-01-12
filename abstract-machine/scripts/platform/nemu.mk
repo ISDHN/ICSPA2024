428be88079fefa1bd04628a2ad3864acaf28223e
ISDHN <231250186@smail.nju.edu.cn>
2025-01-12 13:37:38 +0800

    none

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -22,7 +22,7 @@ insert-arg: image
 	@echo + INSERT-ARG "->" $(IMAGE).bin
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
 
-image: image-dep insert-arg
+image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
